2d34acb90b02143a279999e862d3680b1b18c71d
colobrodo <colognid@gmail.com>
2024-10-22 13:24:13 +0200

    Changed generator type to uint32

--- src/bench_random_access_main.cc
+++ src/bench_random_access_main.cc
@@ -18,7 +18,7 @@ ABSL_FLAG(uint32_t, random, 1000, "Number of random access to randomly selected 
 void TimedRandomAccess(const zuckerli::UncompressedGraph& graph, uint32_t random, uint32_t repeats) {
   std::mt19937 mt();
   mt.seed(0);
-  std::uniform_int_distribution<> distrib(1, random);
+  std::uniform_int_distribution<uint32_t> distrib(0, graph.size() - 1);
   std::vector<bool> samples(random, 0);
   for(uint32_t i = 0; i < random; i++) {
     samples[i] = distrib(mt);
