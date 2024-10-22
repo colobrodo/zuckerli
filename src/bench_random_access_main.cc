1e19967bd91e99b8a31f7aa1c1d63c9e434c4f62
colobrodo <colognid@gmail.com>
2024-10-22 14:33:06 +0200

    Fix names

--- src/bench_random_access_main.cc
+++ src/bench_random_access_main.cc
@@ -38,11 +38,12 @@ void TimedRandomAccess(zuckerli::CompressedGraph graph, uint32_t random, uint32_
     }
     auto t_stop = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration<double, std::nano>(t_stop - t_start).count();
+    std::count << "arcs: " << arcs << std::endl;
     auto time_per_arc = elapsed / arcs;
     std::cout
         << "Wall time elapsed: "
         << elapsed
-        << " ms" << std::endl
+        << " ns" << std::endl
         << "Average random access: "
         << time_per_arc
         << " ns/arc" << std::endl;
