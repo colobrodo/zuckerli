fc6b589ea95384c129bee3777b5618a08e496140
colobrodo <colognid@gmail.com>
2024-10-22 13:39:31 +0200

    Added seed cli parameter

--- src/bench_random_access_main.cc
+++ src/bench_random_access_main.cc
@@ -11,12 +11,12 @@
 #include "uncompressed_graph.h"
 
 ABSL_FLAG(std::string, input_path, "", "Input file path.");
-ABSL_FLAG(uint32_t, repeats, 10, "The number of repeats");
 ABSL_FLAG(uint32_t, random, 1000, "Number of random access to randomly selected node");
+ABSL_FLAG(uint32_t, repeats, 10, "The number of repeats");
+ABSL_FLAG(uint32_t, seed, 0, "The seed of the experiment");
 
-void TimedRandomAccess(zuckerli::CompressedGraph graph, uint32_t random, uint32_t repeats) {
-  std::mt19937 mt;
-  mt.seed(0);
+void TimedRandomAccess(zuckerli::CompressedGraph graph, uint32_t random, uint32_t repeats, uint32_t seed) {
+  std::mt19937 mt(seed);
   std::uniform_int_distribution<uint32_t> distrib(0, graph.size() - 1);
   std::vector<bool> samples(random, 0);
   for(uint32_t i = 0; i < random; i++) {
@@ -52,6 +52,6 @@ int main(int argc, char* argv[]) {
   absl::ParseCommandLine(argc, argv);
   zuckerli::CompressedGraph graph(absl::GetFlag(FLAGS_input_path));
   std::cout << "This graph has " << graph.size() << " nodes." << std::endl;
-  TimedRandomAccess(graph, absl::GetFlag(FLAGS_random), absl::GetFlag(FLAGS_repeats));
+  TimedRandomAccess(graph, absl::GetFlag(FLAGS_random), absl::GetFlag(FLAGS_repeats), absl::GetFlag(FLAGS_seed));
   return 0;
 }
