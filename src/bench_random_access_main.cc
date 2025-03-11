0f3862741609a4a83d39bc9f550dec210dac9871
colobrodo <turbosmi3000@gmail.com>
2025-03-11 16:35:19 +0100

    Added option to format output as tsv

--- src/bench_random_access_main.cc
+++ src/bench_random_access_main.cc
@@ -14,17 +14,19 @@ ABSL_FLAG(std::string, input_path, "", "Input file path.");
 ABSL_FLAG(uint32_t, random, 1000, "Number of random access to randomly selected node");
 ABSL_FLAG(uint32_t, repeats, 10, "The number of repeats");
 ABSL_FLAG(uint32_t, seed, 0, "The seed of the experiment");
+ABSL_FLAG(bool, tsv, false, "Porcellain-like option: If true the output is provided as tsv, with three field:"
+  " the first for the iteration number, followed by the total time of this run and lastly the run's average time (in ns/arc)");
 
-void TimedRandomAccess(zuckerli::CompressedGraph graph, uint32_t random, uint32_t repeats, uint32_t seed) {
+void TimedRandomAccess(zuckerli::CompressedGraph graph, uint32_t random, uint32_t repeats, uint32_t seed, bool print_as_tsv) {
   std::mt19937 mt(seed);
   std::uniform_int_distribution<uint32_t> distrib(0, graph.size() - 1);
   std::vector<uint32_t> samples(random, 0);
-  for(uint32_t i = 0; i < random; i++) {
+  for (uint32_t i = 0; i < random; i++) {
     auto a = distrib(mt);
     samples[i] = a;
   }
 
-  std::cout << "Random access to adjacency lists..." << std::endl;
+  if (!print_as_tsv) std::cout << "Random access to adjacency lists..." << std::endl;
   for (uint32_t repeat = 0; repeat < repeats; repeat++) {
     auto arcs = 0;
     auto t_start = std::chrono::high_resolution_clock::now();
@@ -35,20 +37,30 @@ void TimedRandomAccess(zuckerli::CompressedGraph graph, uint32_t random, uint32_
     auto t_stop = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration<double, std::nano>(t_stop - t_start).count();
     auto time_per_arc = elapsed / arcs;
-    std::cout
-        << "Wall time elapsed: "
-        << elapsed
-        << " ns" << std::endl
-        << "Average random access: "
-        << time_per_arc
-        << " ns/arc" << std::endl;
+    if (print_as_tsv) {
+      std::cout 
+          << repeat << "\t" 
+          << elapsed << "\t" 
+          << time_per_arc << std::endl;
+    } else {
+      std::cout
+          << "Wall time elapsed: "
+          << elapsed
+          << " ns" << std::endl
+          << "Average random access: "
+          << time_per_arc
+          << " ns/arc" << std::endl;
+    }
   }
 }
 
 int main(int argc, char* argv[]) {
   absl::ParseCommandLine(argc, argv);
   zuckerli::CompressedGraph graph(absl::GetFlag(FLAGS_input_path));
-  std::cout << "This graph has " << graph.size() << " nodes." << std::endl;
-  TimedRandomAccess(graph, absl::GetFlag(FLAGS_random), absl::GetFlag(FLAGS_repeats), absl::GetFlag(FLAGS_seed));
+  auto print_as_tsv = absl::GetFlag(FLAGS_tsv);
+  if (!print_as_tsv) {
+    std::cout << "This graph has " << graph.size() << " nodes." << std::endl;
+  }
+  TimedRandomAccess(graph, absl::GetFlag(FLAGS_random), absl::GetFlag(FLAGS_repeats), absl::GetFlag(FLAGS_seed), print_as_tsv);
   return 0;
 }
